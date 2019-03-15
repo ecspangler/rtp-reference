add70a411bec12a77d99b07c2758c924eb4c1fc4
Wheeler Law <wheeler@redhat.com>
2019-03-15 13:09:10 -0400

    Skeleton for the service that collects information from all the topics

---
+++ rtp-flow-viz-service/src/main/java/rtp/demo/visualization/producer/DebtorPaymentsProducer.java
@@ -0,0 +1,35 @@
+package rtp.demo.visualization.producer;
+
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import rtp.demo.debtor.domain.model.payment.Payment;
+
+public class DebtorPaymentsProducer {
+	private static final Logger LOGGER = Logger.getLogger(DebtorPaymentsProducer.class.getName());
+
+	private DebtorPaymentsProducerConfig config;
+	private KafkaProducer<String, Payment> producer;
+	private Properties props;
+
+	public DebtorPaymentsProducer() {
+		this.config = DebtorPaymentsProducerConfig.fromEnv();
+		this.props = DebtorPaymentsProducerConfig.createProperties(config);
+		this.producer = new KafkaProducer<>(props);
+	}
+
+	public void sendMessage(String key, Payment payment) throws InterruptedException {
+		LOGGER.info("Sending message: {}" + payment);
+		producer.send(new ProducerRecord<>(config.getTopic(), key, payment));
+
+		LOGGER.info("Message sent.");
+	}
+
+	public void closeProducer() {
+		producer.close();
+	}
+
+}
