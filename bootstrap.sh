fa30a14e4cc5ee79cd082fcb41a2e04183bcd740
Wheeler Law <wheeler@redhat.com>
2019-03-16 12:20:40 -0400

    Moved more configmaps from the build script to the deploymentconfig

--- bootstrap.sh
+++ bootstrap.sh
@@ -174,179 +174,28 @@ do
     cd ..
 done
 
-cd rtp-debtor-payment-service
-mvn clean fabric8:deploy -Popenshift
-cd ..
-
-cd rtp-debtor-send-payment
-mvn clean fabric8:deploy -Popenshift
-cd ..
-
-cd rtp-mock
-mvn clean fabric8:deploy -Popenshift
-cd ..
-
-cd rtp-creditor-receive-payment
-mvn clean fabric8:deploy -Popenshift
-cd ..
-
-cd rtp-creditor-payment-acknowledgement
-oc create configmap rtp-creditor-payment-acknowledgement-config \
-            --from-literal=BOOTSTRAP_SERVERS="rtp-demo-cluster-kafka-bootstrap:9092" \
-            --from-literal=CREDITOR_PAYMENTS_TOPIC=creditor-payments \
-            --from-literal=MOCK_RTP_CREDITOR_ACK_TOPIC=mock-rtp-creditor-acknowledgement \
-            --from-literal=CONSUMER_MAX_POLL_RECORDS=500 \
-            --from-literal=CONSUMER_COUNT=1 \
-            --from-literal=CONSUMER_SEEK_TO=end \
-            --from-literal=CONSUMER_GROUP=rtp-creditor-payment-acknowledgement \
-            --from-literal=ACKS=1
-mvn fabric8:deploy -Popenshift
-oc set env dc/rtp-creditor-payment-acknowledgement --from configmap/rtp-creditor-payment-acknowledgement-config
-cd ..
-
-cd rtp-creditor-payment-confirmation
-oc create configmap rtp-creditor-payment-confirmation-config \
-            --from-literal=BOOTSTRAP_SERVERS="rtp-demo-cluster-kafka-bootstrap:9092" \
-            --from-literal=CREDITOR_CONFIRMATION_TOPIC=creditor-payment-confirmation \
-            --from-literal=MOCK_RTP_CREDITOR_CONFIRMATION_TOPIC=mock-rtp-creditor-confirmation \
-            --from-literal=CONSUMER_MAX_POLL_RECORDS=500 \
-            --from-literal=CONSUMER_COUNT=1 \
-            --from-literal=CONSUMER_SEEK_TO=end \
-            --from-literal=CONSUMER_GROUP=rtp-creditor-payment-confirmation \
-            --from-literal=ACKS=1
-mvn fabric8:deploy -Popenshift
-oc set env dc/rtp-creditor-payment-confirmation --from configmap/rtp-creditor-payment-confirmation-config
-cd ..
-
-cd rtp-creditor-complete-payment
-oc create configmap rtp-creditor-complete-payment-config \
-            --from-literal=BOOTSTRAP_SERVERS="rtp-demo-cluster-kafka-bootstrap:9092" \
-            --from-literal=CREDITOR_COMPLETED_PAYMENTS_TOPIC=creditor-completed-payments \
-            --from-literal=CREDITOR_PAYMENTS_TOPIC=creditor-payments \
-            --from-literal=CREDITOR_CONFIRMATION_TOPIC=creditor-payment-confirmation \
-            --from-literal=APPLICATION_ID=creditor-complete-payment \
-            --from-literal=CLIENT_ID=creditor-complete-payment-client \
-            --from-literal=DATABASE_URL="jdbc:mysql://mysql-56-rhel7:3306/rtpdb" \
-            --from-literal=DATABASE_USER=dbuser \
-            --from-literal=DATABASE_PASS=dbpass
-mvn fabric8:deploy -Popenshift
-oc set env dc/rtp-creditor-complete-payment --from configmap/rtp-creditor-complete-payment-config
-cd ..
-
-cd rtp-creditor-customer-notification
-oc create configmap rtp-creditor-customer-notification-config \
-            --from-literal=BOOTSTRAP_SERVERS="rtp-demo-cluster-kafka-bootstrap:9092" \
-            --from-literal=CREDITOR_COMPLETED_PAYMENTS_TOPIC=creditor-completed-payments \
-            --from-literal=CONSUMER_MAX_POLL_RECORDS=500 \
-            --from-literal=CONSUMER_COUNT=1 \
-            --from-literal=CONSUMER_SEEK_TO=end \
-            --from-literal=CONSUMER_GROUP=rtp-creditor-customer-notification \
-            --from-literal=ACKS=1
-mvn fabric8:deploy -Popenshift
-oc set env dc/rtp-creditor-customer-notification --from configmap/rtp-creditor-customer-notification-config
-cd ..
-
-cd rtp-creditor-core-banking
-oc create configmap rtp-creditor-core-banking-config \
-            --from-literal=BOOTSTRAP_SERVERS="rtp-demo-cluster-kafka-bootstrap:9092" \
-            --from-literal=CREDITOR_COMPLETED_PAYMENTS_TOPIC=creditor-completed-payments \
-            --from-literal=CONSUMER_MAX_POLL_RECORDS=500 \
-            --from-literal=CONSUMER_COUNT=1 \
-            --from-literal=CONSUMER_SEEK_TO=end \
-            --from-literal=CONSUMER_GROUP=rtp-creditor-core-banking \
-            --from-literal=ACKS=1
-mvn fabric8:deploy -Popenshift
-oc set env dc/rtp-creditor-core-banking --from configmap/rtp-creditor-core-banking-config
-cd ..
-
-cd rtp-creditor-auditing
-oc create configmap rtp-creditor-auditing-config \
-            --from-literal=BOOTSTRAP_SERVERS="rtp-demo-cluster-kafka-bootstrap:9092" \
-            --from-literal=CREDITOR_COMPLETED_PAYMENTS_TOPIC=creditor-completed-payments \
-            --from-literal=CONSUMER_MAX_POLL_RECORDS=500 \
-            --from-literal=CONSUMER_COUNT=1 \
-            --from-literal=CONSUMER_SEEK_TO=end \
-            --from-literal=CONSUMER_GROUP=rtp-creditor-auditing \
-            --from-literal=ACKS=1
-mvn fabric8:deploy -Popenshift
-oc set env dc/rtp-creditor-auditing --from configmap/rtp-creditor-auditing-config
-cd ..
-
-cd rtp-debtor-payment-confirmation
-oc create configmap rtp-debtor-payment-confirmation-config \
-            --from-literal=BOOTSTRAP_SERVERS="rtp-demo-cluster-kafka-bootstrap:9092" \
-            --from-literal=DEBTOR_CONFIRMATION_TOPIC=debtor-payment-confirmation \
-            --from-literal=MOCK_RTP_DEBTOR_CONFIRMATION_TOPIC=mock-rtp-debtor-confirmation \
-            --from-literal=CONSUMER_MAX_POLL_RECORDS=500 \
-            --from-literal=CONSUMER_COUNT=1 \
-            --from-literal=CONSUMER_SEEK_TO=end \
-            --from-literal=CONSUMER_GROUP=rtp-debtor-payment-confirmation \
-            --from-literal=ACKS=1
-mvn fabric8:deploy -Popenshift
-oc set env dc/rtp-debtor-payment-confirmation --from configmap/rtp-debtor-payment-confirmation-config
-cd ..
-
-cd rtp-debtor-complete-payment
-oc create configmap rtp-debtor-complete-payment-config \
-            --from-literal=BOOTSTRAP_SERVERS="rtp-demo-cluster-kafka-bootstrap:9092" \
-            --from-literal=DEBTOR_COMPLETED_PAYMENTS_TOPIC=debtor-completed-payments \
-            --from-literal=DEBTOR_PAYMENTS_TOPIC=debtor-payments \
-            --from-literal=DEBTOR_CONFIRMATION_TOPIC=debtor-payment-confirmation \
-            --from-literal=APPLICATION_ID=debtor-complete-payment \
-            --from-literal=CLIENT_ID=debtor-complete-payment-client \
-            --from-literal=DATABASE_URL="jdbc:mysql://mysql-56-rhel7:3306/rtpdb" \
-            --from-literal=DATABASE_USER=dbuser \
-            --from-literal=DATABASE_PASS=dbpass
-mvn fabric8:deploy -Popenshift
-oc set env dc/rtp-debtor-complete-payment --from configmap/rtp-debtor-complete-payment-config
-cd ..
-
-cd rtp-debtor-customer-notification
-oc create configmap rtp-debtor-customer-notification-config \
-            --from-literal=BOOTSTRAP_SERVERS="rtp-demo-cluster-kafka-bootstrap:9092" \
-            --from-literal=DEBTOR_COMPLETED_PAYMENTS_TOPIC=debtor-completed-payments \
-            --from-literal=CONSUMER_MAX_POLL_RECORDS=500 \
-            --from-literal=CONSUMER_COUNT=1 \
-            --from-literal=CONSUMER_SEEK_TO=end \
-            --from-literal=CONSUMER_GROUP=rtp-debtor-customer-notification \
-            --from-literal=ACKS=1
-mvn fabric8:deploy -Popenshift
-oc set env dc/rtp-debtor-customer-notification --from configmap/rtp-debtor-customer-notification-config
-cd ..
-
-cd rtp-debtor-core-banking
-oc create configmap rtp-debtor-core-banking-config \
-            --from-literal=BOOTSTRAP_SERVERS="rtp-demo-cluster-kafka-bootstrap:9092" \
-            --from-literal=DEBTOR_COMPLETED_PAYMENTS_TOPIC=debtor-completed-payments \
-            --from-literal=CONSUMER_MAX_POLL_RECORDS=500 \
-            --from-literal=CONSUMER_COUNT=1 \
-            --from-literal=CONSUMER_SEEK_TO=end \
-            --from-literal=CONSUMER_GROUP=rtp-debtor-core-banking \
-            --from-literal=ACKS=1
-mvn fabric8:deploy -Popenshift
-oc set env dc/rtp-debtor-core-banking --from configmap/rtp-debtor-core-banking-config
-cd ..
-
-cd rtp-debtor-auditing
-oc create configmap rtp-debtor-auditing-config \
-            --from-literal=BOOTSTRAP_SERVERS="rtp-demo-cluster-kafka-bootstrap:9092" \
-            --from-literal=DEBTOR_COMPLETED_PAYMENTS_TOPIC=debtor-completed-payments \
-            --from-literal=CONSUMER_MAX_POLL_RECORDS=500 \
-            --from-literal=CONSUMER_COUNT=1 \
-            --from-literal=CONSUMER_SEEK_TO=end \
-            --from-literal=CONSUMER_GROUP=rtp-debtor-auditing \
-            --from-literal=ACKS=1
-mvn fabric8:deploy -Popenshift
-oc set env dc/rtp-debtor-auditing --from configmap/rtp-debtor-auditing-config
-cd ..
-
-
-# --- Deploy the visualizer gateway
-cd rtp-flow-viz-service
-oc create configmap rtp-flow-viz-service-config --from-literal=BOOTSTRAP_SERVERS="rtp-demo-cluster-kafka-bootstrap:9092"
-mvn fabric8:deploy -Popenshift
-oc set env dc/rtp-flow-viz-service --from configmap/rtp-flow-viz-service-config
-cd ..
-
+for service in \
+    rtp-creditor-auditing \
+    rtp-creditor-complete-payment \
+    rtp-creditor-core-banking \
+    rtp-creditor-customer-notification \
+    rtp-creditor-payment-acknowledgement \
+    rtp-creditor-payment-confirmation \
+    rtp-creditor-receive-payment \
+    rtp-debtor-auditing \
+    rtp-debtor-complete-payment \
+    rtp-debtor-core-banking \
+    rtp-debtor-customer-notification \
+    rtp-debtor-payment-confirmation \
+    rtp-debtor-payment-service \
+    rtp-debtor-send-payment \
+    rtp-flow-viz-service \
+    rtp-mock
+do
+    printf "Deploying $service\n"
+    cd $service
+    mvn clean fabric8:deploy -Popenshift
+    cd ..
+done
 
 # --- Deploy the web apps
