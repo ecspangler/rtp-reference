ece3cfaf451c64aa06d8995a26ef93d396a0afcd
Wheeler Law <wheeler@redhat.com>
2019-03-17 12:47:58 -0400

    Client payment service is working

--- rtp-creditor-payment-service/src/main/java/rtp/demo/creditor/payments/service/CreditorPaymentService.java
+++ rtp-creditor-payment-service/src/main/java/rtp/demo/creditor/payments/service/CreditorPaymentService.java
@@ -1,17 +1,18 @@
 package rtp.demo.creditor.payments.service;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.StaticHandler;
-
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CreditorPaymentService extends AbstractVerticle {
 
-    private static final Logger LOGGER = Logger.getLogger(CreditorPaymentService.class.getName());
+    private static final Logger LOGGER = LoggerFactory.getLogger(CreditorPaymentService.class.getName());
 
     private WebClient client;
     private Router router;
@@ -36,9 +37,19 @@ public class CreditorPaymentService extends AbstractVerticle {
 
     private void connect(HttpMethod method, String path) {
         router.route(method, path)
-                .handler(context ->
-                        client.request(method, 8080, "rtp-debtor-payment-service", path)
-                                .sendBuffer(context.getBody(), ar -> context.response().end(ar.result().body()))
-                );
+                .handler(context -> {
+                    LOGGER.info(method + " " + path);
+                    LOGGER.info(context.getBodyAsString());
+                    client.request(method, 8080, "rtp-debtor-payment-service", path)
+                            .putHeader("content-type", "application/json; charset=utf-8")
+                            .sendBuffer(context.getBody(), ar -> {
+                        LOGGER.info(" +++ Returned: " + ar.result().statusCode() + " " + ar.result().statusMessage());
+                        LOGGER.info(" +++ " + ar.result().bodyAsString());
+                        context.response().setStatusCode(ar.result().statusCode());
+                        context.response().setStatusMessage(ar.result().statusMessage());
+                        context.response().putHeader(HttpHeaders.CONTENT_TYPE, "application/json; charset=utf-8");
+                        context.response().end(ar.result().body());
+                    });
+                });
     }
 }
