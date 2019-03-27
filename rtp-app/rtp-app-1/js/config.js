e31a84543d6799e31865be97d83db2e8f78ce4b4
Liz Spangler <lizspan@gmail.com>
2019-03-26 20:03:40 -0400

    cleaning up

--- rtp-app/rtp-app-1/js/config.js
+++
@@ -1,8 +0,0 @@
-
-
-var appConfig = {
-    "API": {
-        "transaction": "http://rtp-debtor-payment-service-rtp-reference.apps.anuragsdemo.club/transactions-service/queries/transactions",
-        "payment": "http://rtp-debtor-payment-service-rtp-reference.apps.anuragsdemo.club/payments-service/payments"
-    }
-}
