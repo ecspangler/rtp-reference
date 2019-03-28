3ab0dac03e3a1c67bc81466c450bc7d9d5020ea9
Wheeler Law <wheeler@redhat.com>
2019-03-28 03:24:01 -0400

    Everything works perfectly

--- load_test.py
+++ load_test.py
@@ -1,5 +1,8 @@
-import requests, random, time
-url = "http://rtp-debtor-payment-service-rtp-reference.192.168.42.20.nip.io/payments-service/payments"
+#!/usr/bin/env python3
+import requests, random, time, subprocess
+
+url = "http://rtp-debtor-payment-service-rtp-reference." + subprocess.run(['minishift', 'ip'], stdout=subprocess.PIPE).stdout.decode("UTF-8").strip() + ".nip.io/payments-service/payments"
+print("Connecting to URL: " + url)
 
 for x in range(100):
     data = {"payments":[{"senderAccountNumber":"bob","amount":random.randint(1,101),"receiverFirstName":"bob","receiverLastName":"smith","receiverEmail":"a@a.aom","receiverCellPhone":"null"}]}
