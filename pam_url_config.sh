72ddb082439450a32860db19a302a2c0a10117f9
root <root@bastion.nyc-2da0.internal>
2019-09-09 15:48:37 +0000

    PAM url environment variables

---
+++ pam_url_config.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+bc_host=`oc get routes --selector app=rhpam72-trial-ephemeral --selector service=myapp-kieserver -o jsonpath="{.items[0].spec.host}"`
+
+oc create configmap rtp-creditor-pam-url-http-config \
+            --from-literal=BC_PATH="http://${bootstrap}"
+
+oc create configmap rtp-creditor-pam-url-config \
+            --from-literal=BC_HOST="host=executionUser:Redhat@${bootstrap}"
+
+
+oc set env dc/rtp-account-validation-payment-glue --from configmap/rtp-creditor-pam-url-http-config
+
+oc set env dc/rtp-creditor-complete-case-glue  --from configmap/rtp-creditor-pam-url-http-config
+
+oc set env dc/rtp-creditor-elastic-glue --from configmap/rtp-creditor-pam-url-config
+
+oc set env dc/rtp-creditor-fraud-validation-glue --from configmap/rtp-creditor-pam-url-http-config
+
+oc set env dc/rtp-creditor-payment-received-glue --from configmap/rtp-creditor-pam-url-config
