7f76ac04210715cd30113b5ed6915cb4831a3e6e
ecspangler <oishiikani@gmail.com>
2019-09-15 15:57:00 -0400

    Process updates

--- rtp-creditor-account-validation-glue/src/main/java/rtp/demo/debtor/complete/payment/pojo/BusinessCentralTaskInterface.java
+++ rtp-creditor-account-validation-glue/src/main/java/rtp/demo/debtor/complete/payment/pojo/BusinessCentralTaskInterface.java
@@ -7,10 +7,10 @@ import javax.ws.rs.core.MediaType;
 
 public interface BusinessCentralTaskInterface {
 
-   @PUT
-   @Path("/services/rest/server/containers/RTPProcessingEngine_1.0.0-SNAPSHOT/cases/instances/{caseId}/tasks/Account%20Validation%20Complete")
-   @Consumes(MediaType.APPLICATION_JSON)
-   @Produces(MediaType.APPLICATION_JSON)
+	@PUT
+	@Path("/services/rest/server/containers/RTPProcessingEngine_1.0.0-SNAPSHOT/cases/instances/{caseId}/tasks/RTP%20Payment%20Account%20Validation%20Complete")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 
-   public void triggerAdhocTask(@PathParam("caseId") String caseId, String body);
+	public void triggerAdhocTask(@PathParam("caseId") String caseId, String body);
 }
